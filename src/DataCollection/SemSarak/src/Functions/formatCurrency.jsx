03cfe3a071595cc9eca20b7d16fc1d7418dcdbc5
0xBadawy <mohamedbadawysayed02@gmail.com>
2025-03-21 11:52:53 +0200

    Remove obsolete README files, images, and configuration files from the project

--- src/DataCollection/SemSarak/src/Functions/formatCurrency.jsx
+++
@@ -1,8 +0,0 @@
-const formatCurrency = (value, currency = "EGP", locale = "en-EG") => {
-  return new Intl.NumberFormat(locale, {
-    style: "currency",
-    currency: currency,
-  }).format(value);
-};
-
-export default formatCurrency;
--- src/DataCollection/SemSarak/src/Pages/AddAppartement/Sections.jsx/ImagePreview.jsx
+++
@@ -1,6 +0,0 @@
-
-const ImagePreview = ({ image }) => {
-  return image && <img src={image} alt="Preview" className="mt-2 w-full h-auto border rounded" />;
-};
-
-export default ImagePreview;
